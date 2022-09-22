9e297c9f134fd16b9ff1432ea022159475a7ea61
IWPFI <91381519+IWPFI@users.noreply.github.com>
2022-09-22 21:10:48 +0800

    Form窗体设置成透明
    设置背景透明BackColor和TransparencyKey=Gray【两者设置成同一种颜色即可】

--- Weather/Form1.Designer.cs
+++ Weather/Form1.Designer.cs
@@ -35,6 +35,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(753, 498);
@@ -43,6 +44,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "天气小管家";
+            this.TransparencyKey = System.Drawing.Color.Gray;
             this.ResumeLayout(false);
 
         }
