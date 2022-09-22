dcddbecae479bf4fdc6f532f9be1e89742545ad3
IWPFI <91381519+IWPFI@users.noreply.github.com>
2022-09-22 21:00:25 +0800

    Form窗体居中以及去边框
    1、设置启动时居中-StartPostion=CenterParent
    2、设置无边框-FormBorderStyle=None
    3、设置窗体背景图-Background
    4、设置窗体背景图片位置-BackgroundImageLayout

--- Weather/Form1.Designer.cs
+++ Weather/Form1.Designer.cs
@@ -28,10 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(753, 498);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "天气小管家";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
