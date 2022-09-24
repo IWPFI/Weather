f75d4f7b35883447460d81e653ebba1da4603239
IWPFI <91381519+IWPFI@users.noreply.github.com>
2022-09-24 11:09:42 +0800

    调用api逻辑
    添加Newtonsoft.Json包
    调用web逻辑
    Weathers完善

--- Weather/Form1.cs
+++ Weather/Form1.cs
@@ -1,3 +1,8 @@
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
 using Weather.Model;
 
 namespace Weather
@@ -48,6 +53,50 @@ namespace Weather
                 this.comboBox2.DataSource = currlist;
                 this.comboBox2.SelectedIndex = 0;
             }
+        }
+
+        private void comboBox2_SelectedValueChanged(object sender, EventArgs e)
+        {
+            #region 读取当前的天气
+            if (this.comboBox2.SelectedValue != null)
+            {
+                //接受天气数据
+                Weathers w = new Weathers();
+                using (var client = new WebClient())
+                {
+                    client.Encoding = Encoding.UTF8;
+                    //天气的aip地址
+                    string serviceAddress = "http://api.k780.com/?app=weather.realtime&weaId=1&ag=today,futureDay,lifeIndex,futureHour&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=jsondeeeew";
+                    //通过WebClient实例化对象后，下载网页数据
+                    var data = client.DownloadString(serviceAddress);
+
+                    //将数据序列化为Weathers对象
+                    w = JsonConvert.DeserializeObject<Weathers>(data) ?? new Weathers();
+                }
+
+                if (w.success == "1")
+                {
+                    label1.Text = DateTime.Now.ToString("HH:mm");
+                    label2.Text = w.result.week;
+                    label3.Text = w.result.weather_curr;
+                    label4.Text = w.result.temp_low + "C-" + w.result.temp_high + "C";
+
+                    //设置图片
+                    //pictureBox1.BackgroundImage = Image.FromFile(Application.StartupPath + "/weather/" + w.result.weather_curr + ".png");
+                }
+                else
+                {
+                    if (w.msg == "EXCEED_LIMIT")
+                    {
+                        MessageBox.Show("使用次数超限");
+                    }
+                    else
+                    {
+                        MessageBox.Show("无法查询到当前城市天气消息");
+                    }
+                }
+            }
+            #endregion
         }
     }
 }
---
+++ Weather/Model/Weathers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Model
+{
+    public class Weathers
+    {
+        public string success { get; set; }
+        public Result result { get; set; }
+        public string msg { get; set; }
+    }
+
+    public class CityInfo
+    {
+        public string city { get; set; }
+        public string citykey { get; set; }
+        public string parent { get; set; }
+        public string updateTime { get; set; }
+    }
+
+    public class Forecast
+    {
+        public string date { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+        public string ymd { get; set; }
+        public string weed { get; set; }
+        public string sunrise { get; set; }
+        public string aqi { get; set; }
+        public string fx { get; set; }
+        public string fl { get; set; }
+        public string type { get; set; }
+        public string notice { get; set; }
+    }
+
+    public class Result
+    {
+        public string days { get; set; }
+        public string week { get; set; }
+        public string weather_curr { get; set; }
+        public string temp_high { get; set; }
+        public string temp_low { get; set; }
+        public string citynm { get; set; }
+    }
+}
